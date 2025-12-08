ff32f8e5630726cc40a47144d2f8731556f947e8
poojashree7 <poojashreeb297@gmail.com>
2025-12-08 17:43:35 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,12 @@
+public class example{
+  public static void main(String args[]){
+                            System.out.println("wewlcome to java");
+  int a=5;
+  int b=6;
+  int c;
+  c=a+b;
+  System.out.println("sum of a and b is "+c);
+  }
+}
+                    
+                          
