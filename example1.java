89778a6d6cb416a9b2aec0d5eb06b658e1478a51
poojashree7 <poojashreeb297@gmail.com>
2025-12-08 17:44:01 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class example1{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    String name=sc.next();
+    String dept=sc.next();
+    String age=sc.next();
+    System.out.println("student name "+name);
+    System.out.println("student department "+dept);
+    System.out.println("student age "+age);
+  }
+}
