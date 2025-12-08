8f90c4213336151e0d512e8431801c7847139ff5
poojashree7 <poojashreeb297@gmail.com>
2025-12-08 18:15:54 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class arithmetic{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=a+b;
+    int d=a-b;
+    int e=a*b;
+    System.out.println("addition "+c);
+    System.out.println("subtraction "+d);
+    System.out.println("multiplication "+e);
+  }
+}
+    
